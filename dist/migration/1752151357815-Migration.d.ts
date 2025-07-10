8647dedb65e8b7137d384fae0d5d7b0db5edc221
水野光琉 <99012157+mzunohkaru@users.noreply.github.com>
2025-07-10 22:58:09 +0900

    Delete dist directory

--- dist/migration/1752151357815-Migration.d.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-export declare class Migration1752151357815 implements MigrationInterface {
-    name: string;
-    up(queryRunner: QueryRunner): Promise<void>;
-    down(queryRunner: QueryRunner): Promise<void>;
-}
-//# sourceMappingURL=1752151357815-Migration.d.ts.map
--- dist/utils/password.util.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class PasswordUtil {
-    static hashPassword(password: string): Promise<string>;
-    static comparePassword(password: string, hash: string): Promise<boolean>;
-}
-//# sourceMappingURL=password.util.d.ts.map
